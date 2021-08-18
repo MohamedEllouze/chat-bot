8c4ccda30285217c7b0cd1f205418f8914da6873
Ellouze Mohamed <47985183+MohamedEllouze@users.noreply.github.com>
2021-08-18 17:09:47 +0200

    webhook event

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -11,7 +11,7 @@ let postWebhook = (req, res) => {
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      console.log("webhook event----" + webhook_event);
     });
 
     // Returns a '200 OK' response to all requests
